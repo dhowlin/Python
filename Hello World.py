9112f2ce541f1697c702923e655f86844397beb8
dhowlin <63684554+dhowlin@users.noreply.github.com>
2020-04-14 20:56:02 -0300

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,16 @@
+print ("Hello World")
+print ("Mi nombre es David. Esto es un curso de Python")
+print ("Hello World")
+print ("Hello World")
+
+print("Esto es un mensaje de despedida: ")
+input("Ingrese su mensaje de bienvenida: " + mensaje)
+print(mensaje)
+
+
+
+
+
+
+
+
